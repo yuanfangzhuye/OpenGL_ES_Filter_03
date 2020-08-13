f6242ca585b2e6b1dd57e5290b1ebdfabe7a5d73
chao.li <chao.li@team-lab.cn>
2020-08-13 18:23:50 +0800

    Initial Commit

---
+++ OpenGL_ES_Filter_03/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  OpenGL_ES_Filter_03
+//
+//  Created by tlab on 2020/8/13.
+//  Copyright © 2020 yuanfangzhuye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
